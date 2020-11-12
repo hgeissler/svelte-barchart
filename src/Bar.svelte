0dd0b6b37459491e8728ca09487afdec286f63c2
Hannes Geißler <h.geiler@schille.local>
2020-11-12 15:27:29 +0100

    styling for bar component

--- src/Bar.svelte
+++ src/Bar.svelte
@@ -10,3 +10,44 @@
   </div>
   <p>{value}</p>
 </div>
+
+<style>
+  :root {
+    --bar-height: 10px
+  }
+  
+  .bar {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .bar p {
+    text-align: right;
+  }
+
+  .bar p:first-child {
+    margin-right: 1em;
+    min-width: 4em;
+    width: 10%;
+  }
+
+  .bar p:last-child {
+    margin-left: 1em;
+    min-width: 2.5em;
+  }
+
+  .bar div {
+    height: var(--bar-height);
+  }
+
+  .bar > div {
+    background: #ededed;
+    width: 100%;
+  }
+
+  .bar > div > div {
+    background: #2c393f;
+    display: block;
+  }
+</style>
