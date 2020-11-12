51158c211b5cb950946d4c6d9d346c73de26679e
Hannes Geißler <h.geiler@schille.local>
2020-11-12 21:57:06 +0100

    added randomized recursive test function

--- src/main.js
+++ src/main.js
@@ -16,4 +16,24 @@ characteristics.forEach((entry) => {
 barChart.update('Power', 80)
 barChart.update('Stamina', 50)
 
+// test functions for changing the bar values randomly
+const getRandom = (max = 100, min = 0) => {
+  min = Math.floor(min)
+  max = Math.ceil(max)
+  return Math.round(Math.random() * (max - min)) + min
+}
+
+const updateData = (chart, options, time) => {
+  setTimeout(() => {
+    const index = getRandom(options.length - 1)
+    const option = options[index]
+    const value = getRandom()
+    chart.update(option, value)
+    // recursive
+    updateData(chart, options, time)
+  }, time)
+}
+// changes value of characterisitcs every 2 seconds
+updateData(barChart, characteristics, 2000)
+
 export default barChart
